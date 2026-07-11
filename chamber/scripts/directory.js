1e944e2b335002a240c5d770753abe6195b1341d
tyler-allen03 <tylerjallen57@gmail.com>
2026-07-10 21:47:23 -0500

    made some adjustments

--- chamber/scripts/directory.js
+++ chamber/scripts/directory.js
@@ -10,14 +10,47 @@ navbutton.addEventListener('click', () => {
 
 
 
-const url = 'data/members.json';
+const url = 'https://tyler-allen03.github.io/wdd231/chamber/data/members.json';
 const cards = document.querySelector('#cards');
 
 async function getMemberData() {
     const response = await fetch(url);
     const data = await response.json();
-    console.table(data.members);
-    // displaymembers(data.members);
+    console.table(data.companies);
+    displaymembers(data.companies);
 }
 
 getMemberData();
+
+const displaymembers = (members) => {
+    members.forEach(member => {
+        let card = document.createElement("section");
+        let companyName = document.createElement("h2");
+        let companyLogo = document.createElement("img");
+        let companyAddress = document.createElement("p");
+        let companyEmail = document.createElement("p");
+        let companyPhone = document.createElement("p");
+        let companyURL = document.createElement("a");
+
+        companyName.textContent = member.name;
+        companyEmail.textContent = member.email;
+        companyPhone.textContent = member.phoneNumber;
+        companyAddress.textContent = member.address;
+        companyURL.textContent = member.url;
+
+        companyLogo.setAttribute("src", member.image);
+        companyLogo.setAttribute("alt", `The Logo for ${member.name}`);
+        companyLogo.setAttribute("loading", "lazy");
+        companyLogo.setAttribute("width", "auto");
+        companyLogo.setAttribute("height", "100");
+
+        card.appendChild(companyName);
+        card.appendChild(companyLogo);
+        card.appendChild(companyAddress);
+        card.appendChild(companyEmail)
+        card.appendChild(companyPhone);
+        card.appendChild(companyURL);
+
+        cards.appendChild(card);
+    });
+}
